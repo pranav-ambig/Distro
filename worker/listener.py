2c9d9b963cf225fb5392564d63874059f56c46b9
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-04-20 01:57:22 +0530

    base64 version

--- worker/listener.py
+++ worker/listener.py
@@ -1,5 +1,3 @@
-
-
 from worker import spin_up
 
 import socketio
@@ -8,33 +6,42 @@ workers=[]
 
 from threading import Thread
 
-
 import base64
 
 def decodeData(data):
-    bindata = base64.b64decode(data[0])
+    # print(data)
+    bindata = base64.b64decode(data)
     with open('worker.zip', 'wb') as f:
         f.write(bindata)
-    
 
-# standard Python
-with socketio.SimpleClient() as sio:
-    # ... connect to a server and use the client
-    # ... no need to manually disconnect!
+sio = socketio.Client()
+
+@sio.event
+def connect():
+    print("I'm connected!")
+
+@sio.event
+def disconnect():
+    print("I'm disconnected!")
+
+@sio.on('chunk-upload')
+def on_message(data):
+    print('I received a message!')
+    decodeData(data)
+    spin_up()
+
+try:
     sio.connect('http://172.16.129.26:5000')
-    event = sio.receive()
-    print(f'received event: "{event[0]}" with arguments {event[1:]}')
-    decodeData(event[1:])
+    sio.wait()
+finally:
+    sio.disconnect()
 
 
+# # spawn function calls here
+# wrkr = Thread(target=spin_up)
+# wrkr.start()
+# workers.append(wrkr)
 
-# spawn function calls here
-wrkr = Thread(target=spin_up)
-wrkr.start()
-workers.append(wrkr)
 
-
-for wrkr in workers:
-    wrkr.join()
-    
-    
+# for wrkr in workers:
+#     wrkr.join()
