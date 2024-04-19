aaafe83899af8fbdcc43efba982adcb3671222c8
Sathvik-Malgikar <Sathvik.malgikar@gmail.com>
2024-04-19 21:54:38 +0530

    removed unnecesary stuff

--- worker/listener.py
+++ worker/listener.py
@@ -8,6 +8,15 @@ workers=[]
 
 from threading import Thread
 
+
+import base64
+
+def decodeData(data):
+    bindata = base64.b64decode(data[0])
+    with open('worker.zip', 'wb') as f:
+        f.write(bindata)
+    
+
 # standard Python
 with socketio.SimpleClient() as sio:
     # ... connect to a server and use the client
@@ -15,11 +24,14 @@ with socketio.SimpleClient() as sio:
     sio.connect('http://172.16.129.26:5000')
     event = sio.receive()
     print(f'received event: "{event[0]}" with arguments {event[1:]}')
-    
-    # spawn function calls here
-    wrkr = Thread(target=spin_up)
-    wrkr.start()
-    workers.append(wrkr)
+    decodeData(event[1:])
+
+
+
+# spawn function calls here
+wrkr = Thread(target=spin_up)
+wrkr.start()
+workers.append(wrkr)
 
 
 for wrkr in workers:
